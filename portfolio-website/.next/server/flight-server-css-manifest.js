fe2d3478a6986ddfa8911880db16a9ece70a3ec6
sweetmangoes <alphonsechristian@gmail.com>
2023-06-03 15:18:57 -0400

    update folder name to website

---
+++ portfolio-website/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/christianalphonse/lighthouse/portfolio-website/portfolio-website/src/app/layout.js": [
+      "/Users/christianalphonse/lighthouse/portfolio-website/portfolio-website/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/christianalphonse/lighthouse/portfolio-website/portfolio-website/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/christianalphonse/lighthouse/portfolio-website/portfolio-website/src/app/page": [
+      "/Users/christianalphonse/lighthouse/portfolio-website/portfolio-website/src/app/globals.css",
+      "/Users/christianalphonse/lighthouse/portfolio-website/portfolio-website/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
