17b0ba608beca972b601d80eec8239c383ecd636
sweetmangoes <alphonsechristian@gmail.com>
2023-06-03 16:42:07 -0400

    adjusting styling

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -4,14 +4,16 @@ export default function Home() {
   return (
     <main>
       <section className=' border-color: bg-yellow-500 flex justify-evenly'>
-        <div className='border-color: bg-red-800 flex columns-lg'>
+        <div className='border-color: bg-red-800 flex-col justify-center'>
           <p> 👋  Hi everyone, my name is </p>
-          <h1> Christian Alphonse </h1>
+          <h1 className=''> Christian Alphonse </h1>
           <p> I'm a full stack developer.</p>
         </div>
-        <>
+        <div className='border-color: bg-zinc-600'>
+
           Photo
-        </>
+        </div>
+
       </section>
 
     </main>
